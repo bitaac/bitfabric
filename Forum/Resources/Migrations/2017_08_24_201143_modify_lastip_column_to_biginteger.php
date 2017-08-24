08a5d8f300d86f36d0d9fffa5fbbb3c998bacb92
ekludkristoffer <eklukris@gmail.com>
2017-08-24 22:14:45 +0200

    Change __bitaac_forum_posts.lastip from INT(11) to BIGINT(20)

--- Forum/Resources/Migrations/2017_08_24_201143_modify_lastip_column_to_biginteger.php
+++ Forum/Resources/Migrations/2017_08_24_201143_modify_lastip_column_to_biginteger.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyContentColumnToLongtext extends Migration
+class ModifyLastipColumnToBiginteger extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class ModifyContentColumnToLongtext extends Migration
     public function up()
     {
         Schema::table('__bitaac_forum_posts', function (Blueprint $table) {
-            $table->longText('content')->change();
+            $table->bigInteger('lastip')->change();
         });
     }
 
@@ -26,7 +26,7 @@ class ModifyContentColumnToLongtext extends Migration
     public function down()
     {
         Schema::table('__bitaac_forum_posts', function (Blueprint $table) {
-            $table->string('content')->change();
+            $table->integer('lastip')->change();
         });
     }
 }
